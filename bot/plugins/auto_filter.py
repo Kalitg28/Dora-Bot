058c016f0e0b26a084432ea3b346ef8c6df7f445
MrPurple902 <79639422+MrPurple902@users.noreply.github.com>
2021-11-11 14:45:54 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -39,7 +39,7 @@ async def auto_filter(bot, update:Message):
         await Mfilter.mfilter(text=update.text, group_id=chat_id, bot=bot, update=update)
         return
 
-    mfilter = threading.Thread(target=asyncio.run(), args=Mfilter.mfilter((update.text, chat_id, bot, update)))
+    mfilter = threading.Thread(target=asyncio.run, args=Mfilter.mfilter((update.text, chat_id, bot, update)))
     await mfilter.start()
 
     if re.findall(r"((^\/|^,|^\.|^[\U0001F600-\U000E007F]).*)", update.text):
@@ -65,7 +65,7 @@ async def auto_filter(bot, update:Message):
     if not configs:
         return
     movie = await Helpers.cleanse(update.text)
-    imdb = threading.Thread(target=asyncio.run(), args=Helpers.get_movie(movie))
+    imdb = threading.Thread(target=asyncio.run, args=Helpers.get_movie(movie))
     await imdb.start()
     
     allow_video = True

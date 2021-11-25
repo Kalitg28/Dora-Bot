506b4eed6f17417250bad55e62858ad334f66da7
Jisin Jose <79639422+MrPurple902@users.noreply.github.com>
2021-11-25 13:47:47 +0300

    Myr.

--- bot/buttons.py
+++ bot/buttons.py
@@ -19,7 +19,7 @@ class Buttons():
         "HELP": [
             [InlineKeyboardButton("FILTERS ⇝", callback_data="ignore"),InlineKeyboardButton("Auto 🤖", callback_data="edit_c(AF)"),InlineKeyboardButton("Manual 👨‍💻", callback_data="edit_c(MF)")],
             [InlineKeyboardButton("Connections 🔗", callback_data="edit_c(CONN)"),InlineKeyboardButton("Broadcast 📣", callback_data="edit_c(CAST)")],
-            [InlineKeyboardButton("Batch 🗂️", callback_data="edit_c(BATCH)"),InlineKeyboardButton("Others ⭕", callback_data="edit_c(OTHER)"),]
+            [InlineKeyboardButton("Batch 🗂️", callback_data="edit_c(BATCH)"),InlineKeyboardButton("Others ⭕", callback_data="edit_c(OTHER)")],
             [InlineKeyboardButton("🏡 Home 🏡", callback_data="start")]
         ],
         "AF": [

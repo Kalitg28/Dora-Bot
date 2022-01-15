6461b005cf7f809798c682765619e2ad89a21110
Jisin Jose <79639422+MrPurple902@users.noreply.github.com>
2022-01-15 09:53:55 +0530

    Update settings.py

--- bot/plugins/settings.py
+++ bot/plugins/settings.py
@@ -15,7 +15,7 @@ from bot import VERIFY # pylint: disable=import-error
 
 db = Database()
 
-@Client.on_message(filters.command(["settings"]) & filters.chat(Translation.OWNER_ID), group=4)
+@Client.on_message(filters.command(["settings"]) & filters.chat(Translation.OWNER_ID),prefixes=['!','/'], group=4)
 async def pv_settings(bot, update):
     
     chat_id = 902

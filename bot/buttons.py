e0a62e1acabcf97e06db2618180c6ac3fda2419a
Jisin Jose <79639422+Jisin0@users.noreply.github.com>
2022-03-09 19:25:49 +0530

    Add files via upload

--- bot/buttons.py
+++ bot/buttons.py
@@ -19,7 +19,7 @@ class Buttons():
             [InlineKeyboardButton("⎔ Aᴜᴛᴏ Fɪʟᴛᴇʀ ⎔", callback_data="edit_c(AF)"), InlineKeyboardButton("⎎ Mᴀɴᴜᴀʟ Fɪʟᴛᴇʀs ⎎", callback_data="edit_c(MF)"), InlineKeyboardButton("꩜ Gʟᴏʙᴀʟ Fɪʟᴛᴇʀs ꩜", callback_data="edit_c(MF)")],
             [InlineKeyboardButton("⎙ Bᴀᴛᴄʜ ⎙️", callback_data="edit_c(BATCH)"), InlineKeyboardButton("⌬ Cᴏɴɴᴇᴄᴛɪᴏɴ ⌬", callback_data="edit_c(CONN)"), InlineKeyboardButton("⌥ Bʀᴏᴀᴅᴄᴀsᴛ ⌥", callback_data="edit_c(CAST)")],
             [InlineKeyboardButton("« AᴜᴛᴏDᴇʟᴇᴛᴇ »️", callback_data='edit_c(AUTODEL)'), InlineKeyboardButton("ꔮ Cᴀᴘᴛɪᴏɴ ꔮ", callback_data="edit_c(CAPTION)"), InlineKeyboardButton("✎ Sᴘᴇʟʟᴄʜᴇᴄᴋ ✎️", callback_data='edit_c(SPELL)')],
-            [InlineKeyboardButton("⏣ Oᴛʜᴇʀ ⏣", callback_data="edit_c(OTHER)"), InlineKeyboardButton("⌤ Hᴏᴍᴇ ⌤", callback_data="edit_c(START)"]
+            [InlineKeyboardButton("⏣ Oᴛʜᴇʀ ⏣", callback_data="edit_c(OTHER)"), InlineKeyboardButton("⌤ Hᴏᴍᴇ ⌤", callback_data="edit_c(START)")]
         ],
         "AF": [
             [InlineKeyboardButton("⌬ Cᴏɴɴᴇᴄᴛɪᴏɴ ⌬", callback_data="edit_c(CONN)")],

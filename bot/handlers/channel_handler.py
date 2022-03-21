53b36464f3c57c50e2a12240c25bd1dbb8c28825
Jisin Jose <79639422+Jisin0@users.noreply.github.com>
2022-03-21 15:07:01 +0530

    Add files via upload

--- bot/handlers/channel_handler.py
+++ bot/handlers/channel_handler.py
@@ -5,9 +5,10 @@ from bot.plugins.channel import new_files, new_in_channel
 
 
 
-@Client.on_message(filters.chat([-1001774321778, -1001547869793, -1001647894448]) & ~filters.text, group=5)
+@Client.on_message(filters.chat([-1001774321778, -1001547869793, -1001647894448]) & ~filters.text, group=0)
 async def new_files_manager(bot, update):
 
+    print('hit!')
     await new_files(bot, update)
 
 @Client.on_chat_member_updated(filters.chat(-1001547869793), group=5)
